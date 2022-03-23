1cd4f913f75dcc2c647ce3aa07d554eea468f3d8
Tanviruzzaman <46402667+Tanviruzzaman@users.noreply.github.com>
2022-03-24 01:30:01 +0600

    web-portfolio

---
+++ custom script.js
@@ -0,0 +1,9 @@
+$ (document).ready (function(){
+
+});
+
+$ (window).load (function(){
+    if (screen.width <= 699){
+        document.location="https://tanviruzzaman.github.io/web-portfolio/";
+    }
+});
